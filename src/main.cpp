bda3378980649fa2ac886d162075975a7258a3e0
Shogo Makishima <48924544+shogo-makishima@users.noreply.github.com>
2022-09-06 14:29:07 +0300

    1.0.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <Arduino.h>
 #include "Engine/Engine.hpp"
 
+bool b_isComplete = false;
+Timer relayTimer = Timer(10000, []{
+	digitalWrite(RY4, LOW);
+	b_isComplete = true;
+});
+
 void setup() {
 	Serial.begin(115200);
 
@@ -11,9 +17,12 @@ void setup() {
 	pinMode(RY3, OUTPUT);
 	pinMode(RY4, OUTPUT);
 
+	digitalWrite(RY4, HIGH);
+
 	Serial.println("[DEBUG] Setup has complete!");
 }
 
 void loop() {
 	RelaysController::UpdateRelaysState();
+	if (!b_isComplete) relayTimer.Update();
 }
